22a09619459d839102d02185ed4c7e8b5dde224b
dcamilovelandia <deybi.velandia@bodytechcorp.com>
2022-09-16 07:42:00 -0500

    1) restore cover_thumbnail.dart 2) exported cover_thumbnail.dart

--- lib/gallery_media_picker.dart
+++ lib/gallery_media_picker.dart
@@ -1,2 +1,3 @@
 export 'src/presentation/pages/gallery_media_picker.dart';
 export 'src/data/models/picked_asset_model.dart';
+export 'src/presentation/widgets/cover_thumbnail/cover_thumbnail.dart';
